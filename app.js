9baae46551a551c9edfbe7cb6d1bd63d6f4d487b
FlowerCake13 <lminami23@punahou.edu>
2016-10-13 09:40:18 -1000

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("btn").addEventListener("click", hai);
-
-function hai() {
-var emojiS = ["😂","😶","😮","😍","🤑","🤕","😛","😀","😎","😡","🤓","😇","😞","😑","😙","😝","😜","😖","😨","😭","🤐","😳","😴","😋","😐","😱","😤","😷","😘","😌"];
-    document.getElementById("test").innerHTML = emojiS [Math.floor(Math.random()*emojiS.length)];
-}
