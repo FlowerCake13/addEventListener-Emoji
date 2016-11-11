79964c5d3f142aaa3abd1f1e4a4c7ab7484e72b0
FlowerCake13 <lminami23@punahou.edu>
2016-11-10 15:43:17 -1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -15,5 +15,15 @@
       document.getElementById("emptf").innerHTML = emojiS [Math.floor(Math.random()*emojiS.length)];
                    }
 
-
+  document.getElementById("reset1").addEventListener("click", reset1);
+  function reset1()  {
+    var space = ["Nope", "Hai!"]; 
+      document.getElementById("empty").innerHTML = space [Math.floor(Math.random()*emojiS.space)];
+                   }
+  
+ document.getElementById("reset2").addEventListener("click", reset1);
+  function reset1()  {
+    var space = ["Nope", "Hai!"]; 
+      document.getElementById("emptf").innerHTML = space [Math.floor(Math.random()*emojiS.space)];
+                   }
   
